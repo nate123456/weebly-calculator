0a3c1a71b67090d6865b4ec0ebe94aa791c8e2a6
Nathan Whittle <nathan.whittle1@gmail.com>
2020-03-07 15:16:45 -0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -58,7 +58,7 @@ function SetDisplayValue(element, value, shouldReplace = false) {
     element.text(text.split(':')[0] + ': ' + value)
 
   }
-  
+
   element.show()
 }
 
@@ -66,7 +66,7 @@ function InitializeCalculators() {
   $(".calc.form").each(function() {
     var formMarker = $(this)
     var form = formMarker.closest("form")
-    
+
     form.off("submit");
 
     var id = formMarker.attr("name")
@@ -109,10 +109,10 @@ function InitializeCalculators() {
           "coverage": coverage.val() / 100
         }
       }
-      
+
       var copayMarker = $(".calc.copay." + id)
       var copay = GetMarkerElement(copayMarker)
-      
+
       copay.hide()
 
       displayResults = function(data) {
@@ -140,44 +140,42 @@ function InitializeCalculators() {
 
         var householdMembers = GetMarkerElement(householdMemberMarker, "input[type='radio']:checked")
 
-        var finalIncome = income.val().split('$')[1].replace(',', '')        
-               
-        if (~income.val().toLowerCase().indexOf("less than")){
+        var finalIncome = income.val().split('$')[1].replace(',', '')
+
+        if (~income.val().toLowerCase().indexOf("less than")) {
           finalIncome = 5000
-        }
-        else if (~income.val().toLowerCase().indexOf("more than")){
+        } else if (~income.val().toLowerCase().indexOf("more than")) {
           finalIncome = 101000
         }
-        
+
         return {
           "coverage": coverage.val() / 100,
           "income": finalIncome,
           "householdMembers": householdMembers.val(),
         }
       }
-      
+
       var sessionFeeMarker = $(".calc.session-fee." + id)
       var sessionFee = GetMarkerElement(sessionFeeMarker)
-      
+
       sessionFee.hide()
-      
+
       var monthlyRateMarker = $(".calc.monthly-rate." + id)
       var monthlyRate = GetMarkerElement(monthlyRateMarker)
-      
+
       monthlyRate.hide()
-      
+
       var monthsPayoffMarker = $(".calc.months-payoff." + id)
       var monthsPayoffRate = GetMarkerElement(monthsPayoffMarker)
-      
+
       monthsPayoffRate.hide()
 
       displayResults = function(data) {
-        SetDisplayValue(sessionFee, data.SessionFee, sessionFeeMarker.hasClass('replace'))        
+        SetDisplayValue(sessionFee, data.SessionFee, sessionFeeMarker.hasClass('replace'))
 
-        SetDisplayValue(monthlyRate, data.MonthlyFee, monthlyRateMarker.hasClass('replace'))        
+        SetDisplayValue(monthlyRate, data.MonthlyFee, monthlyRateMarker.hasClass('replace'))
 
         SetDisplayValue(monthsPayoffRate, data.MonthsToPayOff, monthsPayoffMarker.hasClass('replace'))
-
       }
 
     } else {
@@ -192,3 +190,7 @@ function InitializeCalculators() {
 }
 
 InitializeCalculators()
+
+$("#shitty").on('click', function() {
+  $("#form-376540936876382466").submit()
+})
